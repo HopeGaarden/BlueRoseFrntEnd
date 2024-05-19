20cc648613d964d180741d6f4902bdbbaead052d
ThisGrDn <fhqht0638@naver.com>
2024-05-20 00:27:42 +0900

    Edit colors, Add a note page to a motion

--- src/page/Note.jsx
+++ src/page/Note.jsx
@@ -1,18 +1,22 @@
 
 import styles from "./Note.module.css";
 import { useRef, useState } from "react";
-import SegmentedControl from "../components/SegmentedControl";
+import SegmentedControl from "../components/Segment/SegmentedControl";
 import Feed from "../components/NoteComponents/Feed";
 import { useParams, useSearchParams } from "react-router-dom";
 import Footer from "../layouts/Footer";
 import Header from "../components/NoteComponents/Header";
 import SearchTab from "../components/NoteComponents/SearchTab";
+import { motion } from "framer-motion"; 
 
 function Note() {
   const [selectedValue, setSelectedValue] = useState("질병일기");
   const { userID } = useSearchParams;
   const { groupID } = useParams();
-
+  const animationVariants = {
+    initial: { opacity: 0, x: -50 },
+    animate: { opacity: 1, x: 0,},
+};
   return (
     <div className={styles.container}>
       <Header />
@@ -39,7 +43,9 @@ function Note() {
         ]}
       />
       <SearchTab />
-        <Feed className={styles.feeds}/>
+      <motion.div variants={animationVariants}
+                        initial="initial"
+                        animate="animate"><Feed className={styles.feeds}/></motion.div>
         <Footer userID={userID} groupID={groupID} />
     </div>
   );
